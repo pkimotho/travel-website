fd07f8d5624fe27e66dfcc5bd6847ed22d4444e1
Kimotho <plkimotho@gmail.com>
2019-12-11 09:58:35 +0300

    adds styles and aos animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    425: {
+        items: 2
+    },
+    768: {
+        items: 3
+    }
+}
 $(document).ready(function() {
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
@@ -14,6 +28,15 @@ $(document).ready(function() {
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
+    // scroll to top
+    $('.move-up span').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    // AOS Instance
+    AOS.init();
 });
